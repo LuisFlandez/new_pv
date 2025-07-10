8748672ca122eef3778b70e7f85c419541df4270
Esparttac0 <nickolaz45@gmail.com>
2025-07-10 14:26:01 -0400

    nav bar centrado

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ window.addEventListener('DOMContentLoaded', () => {
   const nav = document.getElementById('mainNav');
   const toggleBg = () => {
     if (window.scrollY > 0) {
-      nav.classList.add('bg-dark','navbar-dark');
+      nav.classList.add('bg-dark','navbar-dark', 'text-dark');
       nav.classList.remove('bg-transparent', 'navbar-light');
     } else {
-      nav.classList.add('bg-transparent', 'navbar-light');
-      nav.classList.remove('bg-dark', 'navbar-dark');
+      nav.classList.add('bg-transparent', 'navbar-dark','text-ligth');
+      nav.classList.remove('bg-light', 'navbar-dark');
     }
   };
 
-  nav.classList.add('w-100');
-  
+
   const adjustPosition = () => {
     if (window.scrollY > 0) {
       nav.classList.remove('position-absolute');
