f38005b6dceaf698945389c9084517a6636c015e
Esparttac0 <nickolaz45@gmail.com>
2025-07-10 15:06:14 -0400

    update script

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const nav = document.getElementById('mainNav');
   const toggleBg = () => {
     if (window.scrollY > 0) {
-       nav.classList.add('bg-dark','navbar-dark');
+      nav.classList.add('bg-dark','navbar-dark');
       nav.classList.remove('bg-transparent', 'navbar-light');
     } else {
       nav.classList.add('bg-transparent', 'navbar-light');
