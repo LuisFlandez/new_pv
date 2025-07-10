13ed2ea92b15cb4153b0f8dbcf8988aa6f921eb5
Esparttac0 <nickolaz45@gmail.com>
2025-07-10 11:22:06 -0400

    nav bar funcional

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ window.addEventListener('DOMContentLoaded', () => {
       nav.classList.remove('bg-transparent', 'navbar-light');
     } else {
       nav.classList.add('bg-transparent', 'navbar-light');
-      nav.classList.remove('bg-dark','navbar-dark');
+      nav.classList.remove('bg-dark', 'navbar-dark');
     }
   };
-  toggleBg();
-  window.addEventListener('scroll', toggleBg);
+
+  const adjustPosition = () => {
+    if (window.scrollY > 0) {
+      nav.classList.remove('position-absolute');
+      nav.classList.add('position-fixed', 'top-0', 'w-100');
+    } else {
+      nav.classList.add('position-absolute');
+      nav.classList.remove('position-fixed', 'top-0', 'w-100');
+    }
+  };
+  const onScroll = () => {
+    toggleBg();
+    adjustPosition();
+  };
+
+  onScroll();
+  window.addEventListener('scroll', onScroll);
 });
