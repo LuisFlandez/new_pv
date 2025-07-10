42e9dcb9f91deebe7d4395182767e5ca0778f2c1
Esparttac0 <nickolaz45@gmail.com>
2025-07-10 10:22:53 -0400

    navbar act

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+
 window.addEventListener('DOMContentLoaded', () => {
   const nav = document.getElementById('mainNav');
   const toggleBg = () => {
     if (window.scrollY > 0) {
-      nav.classList.add('bg-light');
+      nav.classList.add('bg-dark');
       nav.classList.remove('bg-transparent');
     } else {
       nav.classList.add('bg-transparent');
-      nav.classList.remove('bg-light');
+      nav.classList.remove('bg-dark');
     }
   };
   toggleBg();
